4c6988a8b8d3bf8f75179d9757c9b4fc8815e3f2
bryan nafegar <bcnafegar@yahoo.com>
2013-04-10 17:22:21 -0500

    added settings file
    moved settings out of game

---
+++ TICSET/TICSET/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using gamePlay;
+
+namespace TICSET
+{
+    class Settings
+    {
+
+         public Player player1;          //First player
+        public Player player2;          //Second Player, these are in arbitrary order
+        public char PieceThatGoesFirst; //X or O, first player
+        public int difficulty;
+
+
+        public Settings(Player p1, Player p2, char firstPiece, int diff)
+        { ///constructor
+            player1 = p1;
+            player2 = p2;
+            PieceThatGoesFirst = firstPiece;
+            difficulty = diff;
+        }
+
+    }
+}
